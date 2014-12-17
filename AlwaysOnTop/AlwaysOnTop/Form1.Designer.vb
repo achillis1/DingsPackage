b8d2cd1903bd6d4d40202766df55bceead5eccd4
Ding Li <dingli@gmail.com>
2014-12-16 22:53:59 -0600

    Commit windows always on top

--- AlwaysOnTop/AlwaysOnTop/Form1.Designer.vb
+++ AlwaysOnTop/AlwaysOnTop/Form1.Designer.vb
@@ -22,9 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(734, 293)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
